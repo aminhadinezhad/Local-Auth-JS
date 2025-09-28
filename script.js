ab192f03a12ae98fdf6d41854113d0a31868821f
Amin <mohammadaminhadinezhad@gmail.com>
2025-09-28 09:48:22 +0330

    create login form

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello world!");
+const userName = document.getElementById("user");
+const userPass = document.getElementById("password");
+const btnLogin = document.querySelector(".btn-login");
+
+console.log(userName);
+console.log(userPass);
+
+const user = {
+  user: "amin_hn",
+  password: 123456000000,
+
+  login: function (user, password) {
+    if (password === this.password && user === this.user) {
+      return "User logged in successfully";
+    } else {
+      return "❌ Login failed: invalid username or password";
+    }
+  },
+};
+
+btnLogin.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  console.log(user.login(userName.value, Number(userPass.value)));
+});
