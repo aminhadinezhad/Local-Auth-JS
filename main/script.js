c4dd416c7d42efa2b37474497c24448dd01aad56
Amin <mohammadaminhadinezhad@gmail.com>
2025-10-17 21:51:51 +0330

    chore: placeholder commit

--- main/script.js
+++ main/script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+(function () {
+  const link = document.querySelector('.user-auth-link');
+  let fillEl = null;
+  const fillScale = 18;
+  const radius = 9;
+
+  function removeFill() {
+    if (!fillEl) return;
+
+    const elToRemove = fillEl;
+    fillEl = null;
+
+    elToRemove.style.opacity = '0';
+  }
+
+  function onPointerUpOrCancelOrLeave() {
+    removeFill();
+  }
+
+  function onPointerDown(e) {
+    if (fillEl) {
+      removeFill();
+    }
+
+    fillEl = document.createElement('span');
+    fillEl.className = 'fill';
+
+    const rect = link.getBoundingClientRect();
+
+    const clickX = e.clientX - rect.left;
+    const clickY = e.clientY - rect.top;
+
+    fillEl.style.left = clickX - radius + 'px';
+    fillEl.style.top = clickY - radius + 'px';
+
+    link.appendChild(fillEl);
+
+    requestAnimationFrame(() => {
+      if (fillEl) {
+        fillEl.style.transform = `scale(${fillScale})`;
+      }
+    });
+
+    window.addEventListener('pointerup', onPointerUpOrCancelOrLeave, {
+      once: true,
+    });
+    link.addEventListener('pointerleave', onPointerUpOrCancelOrLeave, {
+      once: true,
+    });
+    link.addEventListener('pointercancel', onPointerUpOrCancelOrLeave, {
+      once: true,
+    });
+  }
+
+  link.addEventListener('pointerdown', onPointerDown);
+})();
